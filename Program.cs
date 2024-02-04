a4fd9ade95d90e7d79619522ff4a2a4a3f43587c
ZIPcde <votmoyyashik100@gmail.com>
2024-02-04 19:47:39 +0300

    integers initialisation

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello World!");
+int text = 100;
+Console.WriteLine(text);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
+
 
