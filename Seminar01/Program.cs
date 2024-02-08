dd9fe7043f2f6c3205a30c48abef4cd555e714d3
ZIPcde <votmoyyashik100@gmail.com>
2024-02-08 11:32:19 +0300

    add Console.ReadLine:Seminar01

---
+++ Seminar01/Program.cs
@@ -0,0 +1,8 @@
+﻿//Console.Readline
+
+//int double char string bool
+int number = int.Parse(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(number);
+Console.WriteLine(number2);
